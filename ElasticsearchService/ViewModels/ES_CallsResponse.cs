1492ab5532ec2a1e4202e5d6ba7fa75a42f40f62
Jason Williams <smailliwnosaj@yahoo.com>
2019-02-21 14:50:53 -0500

    Delete ES_CallsResponse.cs

--- ElasticsearchService/ViewModels/ES_CallsResponse.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace ElasticsearchService.ViewModels
-{
-    public class ES_CallsResponse
-    {
-        public List<ES_CallResponse> docs { get; set; }
-    }
-}
